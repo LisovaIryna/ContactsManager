6c62d6fcd6f0704306c4166c778366b624c6f159
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-09-01 17:06:52 +0300

    Input Tag Helpers

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
@@ -53,7 +54,14 @@ public class PersonsController : Controller
     public IActionResult Create()
     {
         List<CountryResponse> countries = _countriesService.GetAllCountries();
-        ViewBag.Countries = countries;
+        ViewBag.Countries = countries.Select(temp => new SelectListItem()
+        {
+            Text = temp.CountryName,
+            Value = temp.CountryID.ToString()
+        });
+
+        //new SelectListItem() { Text = "Harsha", Value = "1" }
+        // <option value="1">Harsha</option>
         return View();
     }
 
