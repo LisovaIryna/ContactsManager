1c107360bf06e05233bfa2b0dbec1c1fad3bbf1c
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-09-06 20:43:17 +0300

    Delete View

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -136,7 +136,30 @@ public class PersonsController : Controller
                 Value = temp.CountryID.ToString()
             });
             ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
-            return View();
+            return View(personResponse.ToPersonUpdateRequest());
         }
+    }
+
+    [HttpGet]
+    [Route("[action]/{personID}")]
+    public IActionResult Delete(Guid? personID)
+    {
+        PersonResponse? personResponse = _personsService.GetPersonByPersonID(personID);
+        if (personResponse == null)
+            return RedirectToAction("Index");
+
+        return View(personResponse);
+    }
+
+    [HttpPost]
+    [Route("[action]/{personID}")]
+    public IActionResult Delete(PersonUpdateRequest personUpdateRequest)
+    {
+        PersonResponse? personResponse = _personsService.GetPersonByPersonID(personUpdateRequest.PersonID);
+        if (personResponse == null)
+            return RedirectToAction("Index");
+
+        _personsService.DeletePerson(personUpdateRequest.PersonID);
+        return RedirectToAction("Index");
     }
 }
