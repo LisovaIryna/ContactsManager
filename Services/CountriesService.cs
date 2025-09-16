530e5eca32d4b55829c4ae02e6e03b303ca79006
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-09-16 19:59:21 +0300

    Excel to Database Upload

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,5 +1,7 @@
 ﻿using Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using ServiceContracts;
 using ServiceContracts.DTO;
 
@@ -63,5 +65,44 @@ public class CountriesService : ICountriesService
             return null;
 
         return country_response_from_list.ToCountryResponse();
+    }
+
+    public async Task<int> UploadCountriesFromExcelFile(IFormFile formFile)
+    {
+        MemoryStream memoryStream = new();
+        await formFile.CopyToAsync(memoryStream);
+
+        int countriesInserted = 0;
+
+        using (ExcelPackage excelPackage = new(memoryStream))
+        {
+            ExcelWorksheet workSheet = excelPackage.Workbook.Worksheets["Countries"];
+
+            int rowCount = workSheet.Dimension.Rows;
+
+            for (int row = 2; row <= rowCount; row++)
+            {
+                string? cellValue = Convert.ToString(workSheet.Cells[row, 1].Value);
+
+                if (!string.IsNullOrEmpty(cellValue))
+                {
+                    string countryName = cellValue;
+
+                    if (_db.Countries.Where(temp => temp.CountryName == countryName).Count() == 0)
+                    {
+                        Country country = new()
+                        {
+                            CountryName = countryName
+                        };
+                        _db.Countries.Add(country);
+                        await _db.SaveChangesAsync();
+
+                        countriesInserted++;
+                    }
+                }
+            }
+        }
+
+        return countriesInserted;
     }
 }
