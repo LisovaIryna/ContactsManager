cf6eba3ade80c33bff580397a1dafc8b1ea8a138
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-10-07 20:15:18 +0300

    Controller Unit Test

--- ContactsManager/Controllers/PersonsController.cs
+++ ContactsManager/Controllers/PersonsController.cs
@@ -81,7 +81,7 @@ public class PersonsController : Controller
                 Value = temp.CountryID.ToString()
             });
             ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
-            return View();
+            return View(personAddRequest);
         }
         
         // call the service method
