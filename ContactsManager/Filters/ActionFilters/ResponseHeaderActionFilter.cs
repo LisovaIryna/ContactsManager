811af9a2ad5ac71eaf3016da0c8316c4c191ee95
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2025-11-26 19:00:12 +0200

    Async Filters

--- ContactsManager/Filters/ActionFilters/ResponseHeaderActionFilter.cs
+++ ContactsManager/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -2,33 +2,29 @@
 
 namespace ContactsManager.Filters.ActionFilters;
 
-public class ResponseHeaderActionFilter : IActionFilter, IOrderedFilter
+public class ResponseHeaderActionFilter : IAsyncActionFilter, IOrderedFilter
 {
     private readonly ILogger<ResponseHeaderActionFilter> _logger;
-    private readonly string Key;
-    private readonly string Value;
+    private readonly string _key;
+    private readonly string _value;
 
     public int Order { get; set; }
 
     public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value, int order)
     {
         _logger = logger;
-        Key = key;
-        Value = value;
+        _key = key;
+        _value = value;
         Order = order;
     }
 
-    // before
-    public void OnActionExecuting(ActionExecutingContext context)
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        _logger.LogInformation("{FilterName}.{MethodName} method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuting));
-    }
+        _logger.LogInformation("{FilterName}.{MethodName} method - before", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
 
-    // after
-    public void OnActionExecuted(ActionExecutedContext context)
-    {
-        _logger.LogInformation("{FilterName}.{MethodName} method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuted));
+        await next(); // calls the subsequent filter or action
 
-        context.HttpContext.Response.Headers[Key] = Value;
+        _logger.LogInformation("{FilterName}.{MethodName} method - after", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
+        context.HttpContext.Response.Headers[_key] = _value;
     }
 }
